dd9f96d863c506868ae3151d405808352b84e918
Rohan <rohan@Rohans-MacBook-Air.local>
2024-05-07 18:54:48 +0530

    :tada: added initial admin panel

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,10 +1,175 @@
-import { RecoilRoot } from "recoil";
-import Pages from "./pages/Pages";
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
-export default function App() {
-  return (
-    <RecoilRoot>
-      <Pages />
-    </RecoilRoot>
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+
+
+import Calendar from './pages/Calendar';
+import Chart from './pages/Chart';
+import ECommerce from './pages/Dashboard/ECommerce';
+import FormElements from './pages/Form/FormElements';
+import FormLayout from './pages/Form/FormLayout';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Tables from './pages/Tables';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
+import BusinessForm from './pages/Form/BusinessForm';
+import CoustomerReviewForm from './pages/Form/CoustomerReviewForm';
+import MessageMd from './pages/Form/MessageMd';
+import FaqForm from './pages/Form/FaqForm';
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="eCommerce Dashboard" />
+              <ECommerce />
+            </>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <>
+              <PageTitle title="Calendar" />
+              <Calendar />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile" />
+              <Profile />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-elements"
+          element={
+            <>
+              <PageTitle title="Form Elements" />
+              <FormElements />
+            </>
+          }
+        />
+        <Route
+          path="/forms/business-form"
+          element={
+            <>
+              <PageTitle title="Form Elements" />
+              <BusinessForm />
+            </>
+          }
+        />
+        <Route
+          path="/forms/coutomer-review-form"
+          element={
+            <>
+              <PageTitle title="Form Elements" />
+              <CoustomerReviewForm />
+            </>
+          }
+        />
+
+        <Route
+          path="/forms/message-from-md"
+          element={
+            <>
+              <PageTitle title="Form Elements" />
+              <MessageMd />
+            </>
+          }
+        />
+        <Route
+          path="/forms/faq-form"
+          element={
+            <>
+              <PageTitle title="Form Elements" />
+              <FaqForm />
+            </>
+          }
+        />
+        <Route
+          path="/forms/form-layout"
+          element={
+            <>
+              <PageTitle title="Form Layout" />
+              <FormLayout />
+            </>
+          }
+        />
+        <Route
+          path="/tables"
+          element={
+            <>
+              <PageTitle title="Tables" />
+              <Tables />
+            </>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings" />
+              <Settings />
+            </>
+          }
+        />
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart" />
+              <Chart />
+            </>
+          }
+        />
+        <Route
+          path="/ui/alerts"
+          element={
+            <>
+              <PageTitle title="Alerts" />
+              <Alerts />
+            </>
+          }
+        />
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons" />
+              <Buttons />
+            </>
+          }
+        />
+
+
+      </Routes>
+    </>
   );
 }
+
+export default App;
--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './css/style.css';
+import './css/satoshi.css';
+import 'jsvectormap/dist/css/jsvectormap.css';
+import 'flatpickr/dist/flatpickr.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-)
+);
